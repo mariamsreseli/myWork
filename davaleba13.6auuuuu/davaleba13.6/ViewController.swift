ec00d2267cee9eb94a3a58b6a2704b69097b66e1
Mariam Sreseli <mariamsreseli@Mariams-MacBook-Air.local>
2024-04-06 03:19:53 +0200

    davaleba13

---
+++ davaleba13.6auuuuu/davaleba13.6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  davaleba13.6
+//
+//  Created by Mariam Sreseli on 4/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var cappu: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
