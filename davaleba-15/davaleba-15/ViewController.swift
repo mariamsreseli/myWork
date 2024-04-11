8da17505f156dd160c10ee4bb03cd7b0dc775ba5
Mariam Sreseli <mariamsreseli@Mariams-MacBook-Air.local>
2024-04-11 05:20:25 +0400

    davaleba_15

---
+++ davaleba-15/davaleba-15/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  davaleba-15
+//
+//  Created by Mariam Sreseli on 4/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .purple
+    }
+
+
+}
+
+#Preview {
+    ViewController()
+}
